dd82271d08c76801d337557073d0d71d9e667085
cmario <ccmario2001169@gmail.com>
2024-08-28 18:21:28 -0300

    introduccion y midificacion de archivos

--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
 path('tareas/pendientes/', views.ver_tareas_pendientes, name='ver_tareas_pendientes'),
+path('tareaview/',views.TareaView.as_view(),name='repuesta_tarea'),
+path('formulariotarea/',views.crear_tarea,name="crear_tarea")
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
 # views.py
@@ -8,3 +11,27 @@ def ver_tareas_pendientes(request):
     tareas_pendientes = Tarea.objects.filter(estado='pendiente')
 # Se pasa la lista de tareas a la plantilla
     return render(request, 'pendientes.html', {'tareas': tareas_pendientes})
+
+def crear_tarea(request):
+    contexto={
+        'url':'http://localhost:8000/tareaview/',
+        'estados':['pendiente','completado'] # type: ignore
+    }
+
+    return render(request,'crear_tarea.html',contexto)
+
+
+class TareaView(View):
+    def post(self,request):
+        titulo=request.POST.get("titulo")
+        descripcion=request.POST.get("descripcion")
+        estado=request.POST.get("estado")
+
+        if(titulo != "" and descripcion != "" and estado != "" ):
+            nueva_tarea=Tarea(titulo=titulo,descripcion=descripcion,estado=estado)
+            nueva_tarea.save()
+            return HttpRequest("se envio guardo la tarea con exito",status=201)
+        else:
+            return HttpRequest("error parametros incompletos",estatus=400)
+        
+        
